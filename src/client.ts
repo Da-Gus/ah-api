7fb36d7d96f0bcacad011a8a30cffb24a4937088
Guus van de Wiel <guusvdwiel@gmail.com>
2024-04-10 00:11:08 +0200

    fix: removed unnecessary code

--- src/client.ts
+++ src/client.ts
@@ -1,92 +1,59 @@
-import {Product} from "./product";
+import { Product } from "./product";
 import axios from "axios";
 
-export class AHShopClient{
-    private readonly hostname = "www.ah.nl";
-    private readonly ahProduct: Product;
+export class AHShopClient {
+  private readonly hostname = "www.ah.nl";
+  private readonly ahProduct: Product;
 
-    constructor() {
-        this.ahProduct = new Product(this);
+  constructor() {
+    this.ahProduct = new Product(this);
+  }
+
+  product() {
+    return this.ahProduct;
+  }
+
+  public async getByURL(uri: string) {
+    return await this.get(uri);
+  }
+
+  async get(path: string) {
+    return this.request(path, requestMethod.get);
+  }
+
+  async request(path: string, method: requestMethod) {
+    let tokenData: any;
+    let token: string;
+
+    try {
+      const tokenOptions = {
+        method: "POST",
+        url: "https://api.ah.nl/mobile-auth/v1/auth/token/anonymous",
+        headers: { "content-type": "application/json" },
+        data: JSON.stringify({
+          clientId: "appie",
+        }),
+      };
+
+      tokenData = await axios.request(tokenOptions);
+      token = tokenData.data.access_token;
+    } catch (error) {
+      return error;
     }
 
-    product() {
-        return this.ahProduct;
-    }
+    const options = {
+      method: method,
+      url: "https://api.ah.nl/" + path,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "X-Application": "AHWEBSHOP",
+      },
+    };
 
-    public async getByURL(uri:string){
-        return await this.get(uri);
-    }
-
-    async get (path: string) {
-        return this.request(path, requestMethod.get);
-    }
-
-    async request (path:string, method:requestMethod) {
-        // var options = {
-        //     method,
-        //     hostname: this.hostname,
-        //     path,
-        //     headers: {
-        //         "Content-Type": "application/json",
-        //     },
-        // };
-
-
-        const tokenOptions = {
-            method: 'POST',
-            url: 'https://api.ah.nl/mobile-auth/v1/auth/token/anonymous',
-            headers: {'content-type': 'application/json'},
-            data: JSON.stringify({
-                clientId: "appie"
-            })
-        };
-
-        const tokenData: any = await axios.request(tokenOptions);
-        const token: string = tokenData.data.access_token;
-        console.log(token);
-
-        const options = {
-            method: method,
-            url: 'https://api.ah.nl/' + path,
-            headers: {
-              Authorization: `Bearer ${token}`,
-              'X-Application': 'AHWEBSHOP'
-            }
-          };
-
-        return axios.request(options);
-        // return new Promise((resolve, reject) => {
-        //     var req = https.request(options, function (res) {
-        //         var chunks:any = [];
-    
-        //         res.on("data", function (chunk) {
-        //             chunks.push(chunk);
-        //         });
-    
-        //         res.on("end", function (chunk:any) {
-        //             var body:any = Buffer.concat(chunks);
-                    
-        //             try {
-        //                 try {
-        //                     return resolve(JSON.parse(body));
-        //                 }catch(err:any) {
-        //                     return resolve(body.toString());
-        //                 }
-        //             } catch (error) {
-        //                 return reject(error);
-        //             }
-        //         });
-    
-        //         res.on("error", function (error) {
-        //             return reject(error);
-        //         });
-        //     });
-    
-        //     req.end();
-        // });
-    }
+    return axios.request(options);
+  }
 }
 
 export enum requestMethod {
-    get = "GET",
+  get = "GET",
 }
--- src/product.ts
+++ src/product.ts
@@ -1,14 +1,16 @@
-import {AHShopClient} from "./client";
+import { AHShopClient } from "./client";
 
 export class Product {
+  constructor(protected readonly AHShopClient: AHShopClient) {}
 
-    constructor(protected readonly AHShopClient: AHShopClient) {
-
+  public async getProductByID(id: number): Promise<any> {
+    try {
+      const response: any = await this.AHShopClient.get(
+        `mobile-services/product/detail/v4/fir/${id}`
+      );
+      return response.data;
+    } catch (error) {
+      return error;
     }
-
-    public async getProductByID(id: number): Promise<any> {
-        const response = await this.AHShopClient.get(`mobile-services/product/detail/v4/fir/${id}`);
-        return response.data;
-    }
+  }
 }
-
